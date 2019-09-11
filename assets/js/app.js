4b5cd39ae81f9494d2986e5d7c8a14c529662349
zubayerhimel <zhimel007@gmail.com>
2019-09-12 01:49:47 +0600

    Select generated code and copy selected code from the textarea

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
 
+    $('#copy_text').hide();
     $('#command_form').submit(function (e) {
         e.preventDefault();
+
         var getModelName = $('#model_name').val();
         var getDatabaseName = $('#database_name').val();
         var getControllerName = $('#controller_name').val();
@@ -19,13 +21,18 @@ $(document).ready(function () {
             else {
                 var generatedCommand = "dotnet aspnet-codegenerator controller -name " + getControllerName + " -m " + getModelName + " -dc " + getDatabaseName + " --relativeFolderPath Controllers --useDefaultLayout --referenceScriptLibraries";
             }
-
+            $('#copy_text').show();
             $('#copy_text').text(generatedCommand);
+            $('#copy_text').select();
+            document.execCommand("copy");
 
+            // select text from input field on click
             $("#model_name").click(function () {
+                $(this).select();
+            });
+            $("#getControllerName").click(function () {
                 $(this).select();
             });
         }
     });
-
 });
